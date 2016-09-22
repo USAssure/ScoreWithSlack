f00b7020f3a2ff1c42a7f93a0a805cad5a4b93fa
mikebarksdale <mike.barksdale@gmail.com>
2016-09-22 19:04:32 -0400

    Added files

---
+++ ScoreWithSlack.Api/Factory/ScoreWithSlackServiceFactory.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using ScoreWithSlack.Service;
+using System.Configuration;
+
+namespace ScoreWithSlack.Api.Factory
+{
+    public interface IScoreWithSlackServiceFactory
+    {
+        ScoreWithSlackService CreateService();
+    }
+
+    public class ScoreWithSlackServiceFactory : IScoreWithSlackServiceFactory
+    {
+        public ScoreWithSlackService CreateService()
+        {
+            return new ScoreWithSlackService(new ScoreWithSlackConfig(ConfigurationManager.ConnectionStrings["ScoreWithSlackDb"].ToString()));
+        }
+    }
+}
---
+++ ScoreWithSlack.Service/ScoreWithSlackConfig.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScoreWithSlack.Service
+{
+    public class ScoreWithSlackConfig
+    {
+        public string ConnectionString { get; set; }
+        public int MaxScore { get; set; }
+
+        public ScoreWithSlackConfig(string connectionString)
+        {
+            if (string.IsNullOrEmpty(connectionString))
+                throw new ArgumentNullException(nameof(connectionString));
+
+            MaxScore = 20000; //default
+            ConnectionString = connectionString;
+        }
+    }
+}
